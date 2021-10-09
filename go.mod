1c39ee3eec4fb3635cd0c28f34de69fd7264d012
kasra shirazi <kasra.shirazi.1234@gmail.com>
2021-10-09 13:09:11 +0330

    changed package

--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
-module github.com/kasrashrz/gRPC
+module gRPC
 
 go 1.15
 
 require (
-	golang.org/x/net v0.0.0-20211008194852-3b03d305991f // indirect
-	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20211008145708-270636b82663 // indirect
-	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/grpc v1.41.0
+	google.golang.org/protobuf v1.27.1
 )
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gRPC/greet/greetpb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
-func main(){
-	fmt.Println("Hello, World!")
+type Server struct{}
+
+func main() {
+	fmt.Println("Hi From server")
+	listener, err := net.Listen("tcp", "127.0.0.1:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(server, &Server{})
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
